4eae239dd3b28c1d21321961c727b115bc090db5
noenic <nic.dargazanli@gmail.com>
2022-04-23 18:21:26 +0200

    fin de djikstra

--- Nicolas temp/Tests.py
+++ Nicolas temp/Tests.py
@@ -1,8 +1,10 @@
+from cmath import inf
 import json as js
+from time import sleep
 import pandas as pd
 from math import sin, cos, acos, pi
 import numpy as np
-donneesbus=pd.read_csv('donneesbus.csv',sep=';')
+donneesbus=pd.read_csv(r'./donneesbus.csv',sep=';')
 
 arrets={}
 for c in range (len( donneesbus)):
@@ -113,7 +115,8 @@ def distarc(arret1,arret2):
                 - Si l'arret2 est un successeur de l'arret1 le retour sera la distance a vol d'oiseau entre ces deux arrêts
                 - Si l'arret2 n'est pas un successeur de l'arret1 le retour sera une distance infinie
     """
-    if arret2 in voisin(arret1) :               #Si l'arret2 est un successeur de l'arret1          
+    
+    if arret2 in voisin(arret1) or arret1==arret2 :               #Si l'arret2 est un successeur de l'arret1          
         res=distarrets(arret1,arret2)           #Appel de la fonction calculant la distance des deux arrêts
     else:                                       #Sinon
         res=np.Inf                              #La distance est dite Infinie
@@ -134,32 +137,98 @@ for i in range (len(poids_bus)):                      #On parcours la matrice po
 
 
 
+
+
+def conv(l):
+    r=[]
+    for c in range (len( l)):
+        r.append((nom(l[c]),l[c]))
+    return r
+
+
+def djiksrta2(depart,arrive):
+        dernier_idx=indice_som(depart)
+        pred=[None for _ in range(len(nom_arrets))]
+        pred[indice_som(depart)]=indice_som(depart)
+        vu=[]
+        distance=[np.Inf for _ in range(len(nom_arrets))]
+        distance[indice_som(depart)]=0
+        dernier_idx=indice_som(depart)
+        while dernier_idx!=-1:
+                poids_pred=distance[pred[dernier_idx]]
+                input()
+                dernier_idx=min_exclude(distance,vu)
+                vu.append(dernier_idx)
+                print(nom(dernier_idx)," devient le prochain arret, c'est le pred de",nom(pred[dernier_idx]),"le poids passe de",poids_pred, "à ",poids_pred+distance[dernier_idx])
+                print(nom(dernier_idx), "a pour voisins",voisin(nom(dernier_idx)))
+                for s in voisin(nom(dernier_idx)):
+                    print('distance entre ',nom(dernier_idx),'et',s,distarc(nom(dernier_idx),s)," le poids est de ",distance[dernier_idx]," pour un total de ",distarc(nom(dernier_idx),s)+distance[dernier_idx])
+                    if distance[indice_som(s)]>poids_pred+distarc(nom(dernier_idx),s):
+                        print("         nouveau chemin pour ",s," grace a ",nom(dernier_idx),"il était à",distance[indice_som(s)]," et maintenant il est à ",distance[dernier_idx]+distarc(nom(dernier_idx),s))
+                        distance[indice_som(s)]=distance[dernier_idx]+distarc(nom(dernier_idx),s)
+                        pred[indice_som(s)]=dernier_idx
+                    else: 
+                        pass
+                        print("     ",s, "reste a ",distance[indice_som(s)], " car ", distarc(nom(dernier_idx),s)+distance[dernier_idx] , "n'est pas interessant")
+                print("les sommet ignoré sont ",conv(vu))
+                for i in range(2):
+                    print()
+                
+                
+
+        #print(len(vu))    
+        return distance
+    
+
+def min_exclude(distance,marque):
+    min=-1
+    for i in range (len(distance)):
+
+        if i not in marque:
+            if min==-1:
+                min=i
+            elif distance[i][0]<distance[min][0]:
+                min=i
+    return min
+
+
+
+
+
 def djiksrta(depart,arrive):
-    dernier_idx=indice_som(depart)
-    distance=[np.Inf for _ in range(len(nom_arrets))]
-    poids=0
-    ok=False
-    pred=[None for _ in range(len(nom_arrets))]
-    while not ok:
-        print(distance)
-        for c in voisin(nom(dernier_idx)):
-            ok=False
-            if distance[indice_som(c)]>distarc(nom(dernier_idx),c):
-                ok=True
-                distance[indice_som(c)]=distarc(nom(dernier_idx),c)+poids
-                pred[indice_som(c)]=dernier_idx
-                poids=distance[indice_som(c)]
-        dernier_idx=distance.index(min(distance))
-    distance[dernier_idx]=0
-       
+    
+    distance=[(np.Inf,None) for _ in range(len(nom_arrets))]
+    marque=[indice_som(depart)]
+    distance[indice_som(depart)]=(0,indice_som(depart))
+    arret_actuel=indice_som(depart)
+    while arret_actuel!=indice_som(arrive):
+    #while len(marque)<len(distance):
+        #print(f"l'arret est : {nom(arret_actuel)} pred de {nom(distance[arret_actuel][1])} ")
+        #print(f"{nom(arret_actuel)} a comme voisins  : {voisin(nom(arret_actuel))}")
+        for vivi in voisin(nom(arret_actuel)):
+            if indice_som(vivi) not in marque:
+                if distance[indice_som(vivi)][0]>distance[arret_actuel][0]+distarc(nom(arret_actuel),vivi):
+                    #print(f"NOIVELLE DISTANCE  entre {depart} et {vivi} avant {distance[indice_som(vivi)]} apres {distance[arret_actuel][0]+distarc(nom(arret_actuel),vivi)}")
+                    distance[indice_som(vivi)]=(distance[arret_actuel][0]+distarc(nom(arret_actuel),vivi),arret_actuel)
+        #On dertermine le prochain arrets en fonction du prochain plus proche
+        arret_actuel=min_exclude(distance,marque)
+        marque.append(arret_actuel)
+
+    #-------------------------------reconstruction---------------------------------------
+    chemin=[arrive]
+    arret_actuel=indice_som(arrive)
+    while arret_actuel!=indice_som(depart):
+        pred=distance[arret_actuel][1]
+        arret_actuel=pred
+        chemin.append(nom(pred))
+
+    chemin.reverse()
+    return (chemin,distance[indice_som(arrive)][0])
+
             
-    
+  
 
 
-    
-    
+print(djiksrta('NOVE','CHOI'))
 
-        
-    
 
-djiksrta('NOVE','MURG')
