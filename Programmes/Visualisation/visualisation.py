b76eef05acf704f9279ebb48a2de1bfa20dbcfee
Alexandre Maurice <hizaaknewton@gmail.com>
2022-05-19 10:49:04 +0200

    Carte fond

--- Programmes/Visualisation/visualisation.py
+++ Programmes/Visualisation/visualisation.py
@@ -1,7 +1,11 @@
 from graphics import *
 from methodes import *
 
-win = GraphWin("Réseau Chronoplus", 910, 910)
+win = GraphWin("Réseau Chronoplus", 1000, 1000)
+
+fond_carte = Image(Point(495,480), "fond_carte.png")
+fond_carte.draw(win)
+
 
 minX=np.Inf
 minY=np.Inf
@@ -21,16 +25,19 @@ for i in arrets:
 zone = 900
 
 def dessinerGraphe():
-    ratio = zone/(maxX-minX)
+    ratio = zone/(maxX-minX)+400
+    print(ratio)
+    delta=870
+    facteur_Y=7000
     for i in arrets:    
         for j in arrets[i][2]:
             Line(Point((arrets[i][1]-minX)*ratio+5,
-                        700-(arrets[i][0]-minY)*ratio+5),
+                        delta-(arrets[i][0]-minY)*facteur_Y+5),
                  Point((arrets[j][1]-minX)*ratio+5,
-                        700-(arrets[j][0]-minY)*ratio+5)).draw(win)
+                        delta-(arrets[j][0]-minY)*facteur_Y+5)).draw(win)
     for i in arrets:
         Circle(Point((arrets[i][1]-minX)*ratio+5,
-                      700-(arrets[i][0]-minY)*ratio+5),4).draw(win)
+                      delta-(arrets[i][0]-minY)*facteur_Y+5),4).draw(win)
 
 
 def main():
